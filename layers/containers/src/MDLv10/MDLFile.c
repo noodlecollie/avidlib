20497d02df3240bf7e85ff402f6230a774443a4e
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2021-04-22 12:09:31 +0100

    Added skeleton containers lib, improved CMake scripts

---
+++ layers/containers/src/MDLv10/MDLFile.c
@@ -0,0 +1,5 @@
+#include "AVIDLib_Containers/MDLv10/MDLFile.h"
+
+void ALC_MDLv10_Test()
+{
+}
---
+++ layers/containers/unit_mdlv10.cmake
@@ -0,0 +1,32 @@
+function(init_unit_mdlv10 headers sources)
+	set(CONTAINERS_MDLV10 "NO" CACHE BOOL "Includes MDLv10 code in containers library.")
+
+	set(HEADERS_MDLV10
+		include/${TARGETNAME_CONTAINERS}/MDLv10/MDLFile.h
+		PARENT_SCOPE
+	)
+
+	set(SOURCES_MDLV10
+		src/MDLv10/MDLFile.c
+	)
+
+	if(CONTAINERS_MDLV10)
+		set(${headers}
+			${${headers}}
+			${HEADERS_MDLV10}
+			PARENT_SCOPE
+		)
+
+		set(${sources}
+			${${sources}}
+			${SOURCES_MDLV10}
+			PARENT_SCOPE
+		)
+	endif()
+endfunction()
+
+macro(install_unit_mdlv10)
+	if(CONTAINERS_MDLV10)
+		install(FILES ${HEADERS_MDLV10} DESTINATION ${TARGETNAME_CONTAINERS}/include/${TARGETNAME_CONTAINERS}/MDLv10)
+	endif()
+endmacro()

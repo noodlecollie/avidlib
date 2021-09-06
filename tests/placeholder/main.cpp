54c6215a2e3860e1950cbadd6341f7e4194c5ad0
Vesper <jonathan.poncelet@talk21.com>
2021-09-06 08:23:24 +0100

    Fixed placeholder test formatting

--- tests/placeholder/main.cpp
+++ tests/placeholder/main.cpp
@@ -3,14 +3,16 @@
 #include <vector>
 #include <algorithm>
 
-int main() {
-  rc::check("double reversal yields the original value",
-            [](const std::vector<int> &l0) {
-              auto l1 = l0;
-              std::reverse(begin(l1), end(l1));
-              std::reverse(begin(l1), end(l1));
-              RC_ASSERT(l0 == l1);
-            });
+int main()
+{
+	rc::check("double reversal yields the original value",
+		[](const std::vector<int> &l0)
+		{
+			auto l1 = l0;
+			std::reverse(begin(l1), end(l1));
+			std::reverse(begin(l1), end(l1));
+			RC_ASSERT(l0 == l1);
+		});
 
-  return 0;
+	return 0;
 }
